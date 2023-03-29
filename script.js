958c1414243ae8b3cea54c334e3d798a8196ed0d
WreckingGamer <isaachh178@gmail.com>
2023-03-29 07:45:06 -0500

    Separated html and js files

---
+++ script.js
@@ -0,0 +1,27 @@
+var countDownDate = new Date("May 26, 2023 15:15:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("clock").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("clock").innerHTML = "FREE";
+  }
+}, 1000);
+
+function retrieveDate() {
+    const month = getDate().toLocaleString('default', { month: 'long' });
+    document.getElementById("date").innerHTML = getDate().toString() + month + getFullYear().toString();
+    console.log(test);
+};
